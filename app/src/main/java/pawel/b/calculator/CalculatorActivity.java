6ec02121a649004626eca90ca8837cf620384913
Pawel Borkowski <pa7wel@gmail.com>
2016-05-19 07:29:33 +0200

    v1 - Dodanie przycisków i wyświetlacza w layout [activity_calculator.xml].

---
+++ app/src/main/java/pawel/b/calculator/CalculatorActivity.java
@@ -0,0 +1,18 @@
+package pawel.b.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    public void onClicked(View view){
+
+    }
+}
